1133c0ceb528540d98d5ebb0db91880b050c3e40
Lau Mercadal <lau.mercadal@bsc.es>
2015-12-23 13:52:39 +0100

    MB Power to PRV converter
    First commit

---
+++ src/parseOptions.h
@@ -0,0 +1,31 @@
+#ifndef PARSEOPTIONS_H
+#define PARSEOPTIONS_H
+
+#include <popt.h>
+#include <string.h>
+#include <errno.h>
+
+enum
+{
+	OPT_NONE = 0,
+	OPT_MERGE_PRV_FILE,
+	OPT_PWR_FILE,
+	OPT_OUTPUT,
+	OPT_OFFSET,
+	OPT_VERBOSE
+};
+
+static int parseOptions(int argc, char **argv);
+
+static struct poptOption long_options[] =
+{
+	{"merge-prv", 'm', POPT_ARG_STRING, NULL, OPT_MERGE_PRV_FILE, "Merge with paraver trace", NULL},
+	{"power", 'p', POPT_ARG_STRING, NULL, OPT_PWR_FILE, "Power trace", NULL},
+	{"output", 'o', POPT_ARG_STRING, NULL, OPT_OUTPUT, "Output filename", NULL},
+	{"offset", 't', POPT_ARG_INT, NULL, OPT_OFFSET, "Initial timestamp", NULL},
+	{"verbose", 'v', POPT_ARG_NONE, NULL, OPT_VERBOSE, "Be verbose", NULL},
+	POPT_AUTOHELP
+	{NULL, 0, 0, NULL, 0}
+};
+
+#endif
---
+++ src/prvpwr.h
@@ -0,0 +1,11 @@
+#ifndef PRVPWR_H
+#define PRVPWR_H
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <libgen.h>
+
+static void addPCFType(char *file);
+
+#endif
