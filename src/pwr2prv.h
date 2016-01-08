8e74e313fa75bf84d4c93c603f4ec5fe897d2729
Lau Mercadal <lau.mercadal@bsc.es>
2016-01-08 16:52:52 +0100

    Start implementing merge option

---
+++ src/pwr2prv.h
@@ -0,0 +1,12 @@
+#ifndef PRVPWR_H
+#define PRVPWR_H
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <libgen.h>
+
+static void mergeTraces(char *prv, char *pwr);
+static void addPCFType(char *file);
+
+#endif
